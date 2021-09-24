8cdb886a0dc21837584f71866e9795f2f2570406
MatanNoach <61862949+MatanNoach@users.noreply.github.com>
2021-09-24 20:53:23 +0300

    mapping SideBarMenu

--- src/components/SidebarMenu.js
+++ src/components/SidebarMenu.js
@@ -4,47 +4,39 @@ import { Link as RouterLink } from "react-router-dom";
 import { ListItem, List } from "@material-ui/core";
 import { ListItemIcon } from "@material-ui/core";
 import { ListItemText } from "@material-ui/core";
-
+import { Box } from "@mui/system";
 import { TrendingUp } from "@material-ui/icons";
 import FaceIcon from "@material-ui/icons/Face";
-import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
-import MultilineChartIcon from '@mui/icons-material/MultilineChart';
+import CompareArrowsIcon from "@material-ui/icons/CompareArrows";
+import MultilineChartIcon from "@mui/icons-material/MultilineChart";
+import wrappedPages from "../Pages";
+const mapNamesToIcons = new Map();
+mapNamesToIcons.set("Trending", { icon: <TrendingUp />, order: 1 });
+mapNamesToIcons.set("Compare", { icon: <CompareArrowsIcon />, order: 2 });
+mapNamesToIcons.set("My Stocks", { icon: <MultilineChartIcon />, order: 3 });
+mapNamesToIcons.set("Profile", { icon: <FaceIcon />, order: 4 });
 function ListItemLink(props) {
   return <ListItem button component={RouterLink} {...props} />;
 }
 class SidebarMenu extends Component {
   render() {
     return (
-      <List>
-        <ListItemLink to="/trending">
-          <ListItemIcon>
-            <TrendingUp />
-          </ListItemIcon>
-          <ListItemText primary="Trending" />
-        </ListItemLink>
-
-        <ListItemLink to="/myStocks">
-          <ListItemIcon>
-            <MultilineChartIcon/>
-          </ListItemIcon>
-          <ListItemText primary="My Stocks" />
-        </ListItemLink>
-
-
-        <ListItemLink to="/compare">
-          <ListItemIcon>
-            <CompareArrowsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Compare" />
-        </ListItemLink>
-
-        <ListItemLink to="/profile">
-          <ListItemIcon>
-            <FaceIcon />
-          </ListItemIcon>
-          <ListItemText primary="Profile" />
-        </ListItemLink>
-      </List>
+      <Box component={List} display="flex" sx={{flexDirection:"column"}}>
+        {wrappedPages.map((p, i) => {
+          if (mapNamesToIcons.has(p.name)) {
+            return (
+              <Box key={i} sx={{ order: mapNamesToIcons.get(p.name).order }}>
+                <ListItemLink to={p.path}>
+                  <ListItemIcon>
+                    {mapNamesToIcons.get(p.name).icon}
+                  </ListItemIcon>
+                  <ListItemText primary={p.name} />
+                </ListItemLink>
+              </Box>
+            );
+          }
+        })}
+      </Box>
     );
   }
 }
